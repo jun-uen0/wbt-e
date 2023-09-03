6cf0c7c43b5db4471ccb5b9ab8fe47b29db44d67
Jun Ueno <hello@junueno.dev>
2023-09-03 23:42:17 +0900

    add: test donwload from s3

--- lambda/tests/unit/test_handler.py
+++ lambda/tests/unit/test_handler.py
@@ -1,72 +1,27 @@
-import json
-
-import pytest
-
+import unittest
+from unittest.mock import Mock, patch
 from hello_world import app
 
+class TestDownloadDataFromS3(unittest.TestCase):
 
-@pytest.fixture()
-def apigw_event():
-    """ Generates API GW Event"""
+  @patch('hello_world.app.s3_client')
+  def test_download_data_from_s3(self, mock_s3_client):
+    # Set up the mock
+    bucket_name = 'test-bucket'
+    object_key = 'test-key'
+    csv_data = 'line1\nline2\nline3'
+    response = {'Body': Mock(read=Mock(return_value=csv_data.encode('utf-8')))}
+    mock_s3_client.get_object.return_value = response
 
-    return {
-        "body": '{ "test": "body"}',
-        "resource": "/{proxy+}",
-        "requestContext": {
-            "resourceId": "123456",
-            "apiId": "1234567890",
-            "resourcePath": "/{proxy+}",
-            "httpMethod": "POST",
-            "requestId": "c6af9ac6-7b61-11e6-9a41-93e8deadbeef",
-            "accountId": "123456789012",
-            "identity": {
-                "apiKey": "",
-                "userArn": "",
-                "cognitoAuthenticationType": "",
-                "caller": "",
-                "userAgent": "Custom User Agent String",
-                "user": "",
-                "cognitoIdentityPoolId": "",
-                "cognitoIdentityId": "",
-                "cognitoAuthenticationProvider": "",
-                "sourceIp": "127.0.0.1",
-                "accountId": "",
-            },
-            "stage": "prod",
-        },
-        "queryStringParameters": {"foo": "bar"},
-        "headers": {
-            "Via": "1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)",
-            "Accept-Language": "en-US,en;q=0.8",
-            "CloudFront-Is-Desktop-Viewer": "true",
-            "CloudFront-Is-SmartTV-Viewer": "false",
-            "CloudFront-Is-Mobile-Viewer": "false",
-            "X-Forwarded-For": "127.0.0.1, 127.0.0.2",
-            "CloudFront-Viewer-Country": "US",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            "Upgrade-Insecure-Requests": "1",
-            "X-Forwarded-Port": "443",
-            "Host": "1234567890.execute-api.us-east-1.amazonaws.com",
-            "X-Forwarded-Proto": "https",
-            "X-Amz-Cf-Id": "aaaaaaaaaae3VYQb9jd-nvCd-de396Uhbp027Y2JvkCPNLmGJHqlaA==",
-            "CloudFront-Is-Tablet-Viewer": "false",
-            "Cache-Control": "max-age=0",
-            "User-Agent": "Custom User Agent String",
-            "CloudFront-Forwarded-Proto": "https",
-            "Accept-Encoding": "gzip, deflate, sdch",
-        },
-        "pathParameters": {"proxy": "/examplepath"},
-        "httpMethod": "POST",
-        "stageVariables": {"baz": "qux"},
-        "path": "/examplepath",
-    }
+    # Call the function under test
+    result = app.download_data_from_s3(bucket_name, object_key)
 
+    # Assert that the mock was called correctly
+    mock_s3_client.get_object.assert_called_once_with(Bucket=bucket_name, Key=object_key)
 
-def test_lambda_handler(apigw_event):
+    # Assert the function's return value
+    expected_result = ['line1', 'line2', 'line3']
+    self.assertEqual(result, expected_result)
 
-    ret = app.lambda_handler(apigw_event, "")
-    data = json.loads(ret["body"])
-
-    assert ret["statusCode"] == 200
-    assert "message" in ret["body"]
-    assert data["message"] == "hello world"
+if __name__ == '__main__':
+  unittest.main()
