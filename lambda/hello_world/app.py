48932673c419453c30f4f85cb2237c934cf8b607
Jun Ueno <hello@junueno.dev>
2023-09-03 23:39:50 +0900

    move: lambda code

--- lambda/hello_world/app.py
+++ lambda/hello_world/app.py
@@ -1,42 +1,117 @@
 import json
+import boto3
+import csv
+from datetime import datetime, timedelta
 
-# import requests
+s3_client = boto3.client('s3')
+kinesis_client = boto3.client('kinesis')
+dynamodb_client = boto3.client('dynamodb')
+DYNAMO = boto3.resource('dynamodb')
 
+def download_data_from_s3(bucket_name, object_key):
+  response = s3_client.get_object(Bucket=bucket_name, Key=object_key)
+  csv_data = response['Body'].read().decode('utf-8').splitlines()
+  return csv_data
+
+def publish_data_to_kinesis(stream_name, data):
+  response = kinesis_client.put_record(
+    StreamName=stream_name,
+    Data=json.dumps(data),
+    PartitionKey='partition-key'
+  )
+  return response
+
+def aggregate_county_data(records):
+  county_counts = {}
+  for record in records:
+    data = json.loads(record['Data'])
+    county = data.get('County')
+    if county:
+      county_counts[county] = county_counts.get(county, 0) + 1
+  return county_counts
+
+def update_dynamodb(table_name, county_counts):
+  table = DYNAMO.Table(table_name)
+  with table.batch_writer() as batch:
+    for county, count in county_counts.items():
+      batch.put_item(
+        Item={
+          'County': county,
+          'Count': count
+        }
+      )
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+  ## Prod
+  # bucket_name = 'wbt-e-s3-bucket-prod'
+  # object_key = 'recommended-fishing-rivers-and-streams-1.csv'
+  # stream_name = 'wbt-e-kds-prod'
+  # dynamodb_table_name = 'wbt-e-dynamodb-prod'
+	## Develop
+  bucket_name = 'wbt-e-s3-bucket-dev'
+  object_key = 'recommended-fishing-rivers-and-streams-1.csv'
+  stream_name = 'wbt-e-kds-dev'
+  dynamodb_table_name = 'wbt-e-dynamodb-dev'
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
+  csv_data = download_data_from_s3(bucket_name, object_key)
 
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+  # Process and publish data to Kinesis
+  for row in csv.reader(csv_data):
+    waterbody_name = row[0]
+    fish_species = row[1]
+    comments = row[2]
+    regulations_link = row[3]
+    county = row[4]
+    access_types = row[5]
+    access_owner = row[6]
+    waterbody_info = row[7]
+    longitude = row[8]
+    latitude = row[9]
+    location = row[10]
 
-    context: object, required
-        Lambda Context runtime methods and attributes
+    # Add timestamp column with the current datetime
+    timestamp = datetime.now().isoformat()
 
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
+    data = {
+      'WaterbodyName': waterbody_name,
+      'FishSpecies': fish_species,
+      'Comments': comments,
+      'RegulationsLink': regulations_link,
+      'County': county,
+      'AccessTypes': access_types,
+      'AccessOwner': access_owner,
+      'WaterbodyInfo': waterbody_info,
+      'Longitude': longitude,
+      'Latitude': latitude,
+      'Location': location,
+      'Timestamp': timestamp  # Add the timestamp column
     }
+
+    publish_data_to_kinesis(stream_name, data)
+
+  # Get data from Kinesis and aggregate county counts
+  current_time = datetime.utcnow()
+  five_minutes_ago = current_time - timedelta(minutes=5)
+  shard_iterator = kinesis_client.get_shard_iterator(
+    StreamName=stream_name,
+    ShardId='shardId-000000000000',
+    ShardIteratorType='AT_TIMESTAMP',
+    Timestamp=five_minutes_ago
+  )['ShardIterator']
+
+  records = []
+  response = kinesis_client.get_records(
+    ShardIterator=shard_iterator,
+    Limit=100
+  )
+  records.extend(response['Records'])
+
+  county_counts = aggregate_county_data(records)
+
+  # Update DynamoDB
+  update_dynamodb(dynamodb_table_name, county_counts)
+
+  return {
+    'statusCode': 200,
+    'body': json.dumps('Data publishing completed.')
+  }
