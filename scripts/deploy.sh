c98ff6709d0525f6a1746aee1bca297c50a4e5f8
Jun Ueno <hello@junueno.dev>
2023-09-05 19:02:04 +0900

    add: deploy s3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,18 +2,22 @@
 
 # Change current diretory to `scripts/`
 cd $(dirname $0)
-cd ../lambda/
+cd ../cfn/
 
-# Check if SAM version is >= required_sam_version
-sam_version=$(sam --version 2>&1 | awk '/SAM CLI/{print $4}')
-required_sam_version="1.82.0" # Minimum version
-if [ "$(printf '%s\n' "$required_sam_version" "$sam_version" | sort -V | head -n1)" != "$required_sam_version" ]; then
-  echo "Error: SAM CLI version should be $required_sam_version or higher."
+# Check if aws-cli version is >= required_aws_cli_version
+aws_cli_version=$(aws --version 2>&1 | awk '/aws-cli/{print $1}' | cut -d/ -f2)
+required_aws_cli_version="1.10" # Minimum version
+if [ "$(printf '%s\n' "$required_aws_cli_version" "$aws_cli_version" | sort -V | head -n1)" != "$required_aws_cli_version" ]; then
+  echo "Error: aws-cli version should be $required_aws_cli_version or higher."
   exit 1
 fi
 
-# Deploy application
-read -p "Do you deploy application? (y/N):" yn
-case "$yn" in [yY]*) ;; *) echo "Exit" ; exit ;; esac
+# Deploy S3
+aws cloudformation deploy \
+  --template-file s3.yml \
+  --stack-name sample-stack \
+  --s3-prefix prod \
+  --parameter-overrides Env=prod
 
-sam deploy --guided
+# Deploy Kinesis Data steam
+# Deploy 
