c65ae0fc02f7d5990910ac471389834692a89afc
Jun Ueno <hello@junueno.dev>
2023-09-05 19:47:52 +0900

    add: deploy kds, dynamodb

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,11 +13,28 @@ if [ "$(printf '%s\n' "$required_aws_cli_version" "$aws_cli_version" | sort -V |
 fi
 
 # Deploy S3
+echo "Creating stack s3-${env}"
 aws cloudformation deploy \
   --template-file s3.yml \
-  --stack-name sample-stack \
-  --s3-prefix prod \
-  --parameter-overrides Env=prod
+  --stack-name s3-${env} \
+  --s3-prefix ${env} \
+  --parameter-overrides Env=${env}
 
-# Deploy Kinesis Data steam
-# Deploy 
+echo "Update file (csv) for e2e testing to S3 bucket created"
+aws s3 cp ${testfile_path} s3://wbt-e-s3-bucket-e2e/
+
+# Deploy Kinesis Data Stream
+echo "Creating stack kds-${env}"
+aws cloudformation deploy \
+  --template-file kds.yml \
+  --stack-name kds-${env} \
+  --s3-prefix ${env} \
+  --parameter-overrides Env=${env}
+
+# Deploy DynamoDB
+echo "Creating stack dynamodb-${env}"
+aws cloudformation deploy \
+  --template-file dynamodb.yml \
+  --stack-name dynamodb-${env} \
+  --s3-prefix ${env} \
+  --parameter-overrides Env=${env}
