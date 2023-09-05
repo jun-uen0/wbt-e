422081addcc8596c98e8f96dff43f70818aec273
Jun Ueno <hello@junueno.dev>
2023-09-05 19:48:35 +0900

    add: arguments-check

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
+set -e
+
+# Get arguments and define enviroment
+usage() {
+  echo "Please specify arguments after the command:"
+  echo "Deploy to the development environment: ./deploy.sh dev"
+  echo "Deploy to the e2e environment: ./deploy.sh e2e"
+  echo "Deploy to the production environment: ./deploy.sh prod"
+}
+
+if [ -z "$1" ]; then
+  usage
+  exit
+fi
+
+case "$1" in "dev" | "e2e" | "prod" ) ;; * )
+  usage
+  echo "Invalid argument. Allowed arguments are 'dev', 'e2e', and 'prod'."
+  exit 1 ;;
+esac
+
+if [ "$1" == "prod" ]; then
+  read -p "This command will deploy to the production environment. Are you sure? (y/N): " yn
+  case "$yn" in [yY]*) ;; *) echo "Exiting" ; exit ;; esac
+fi
+
+# Variables
+env=$1
+testfile_path=../lambda/tests/e2e/test-files/test.csv
+
 # Change current diretory to `scripts/`
 cd $(dirname $0)
 cd ../cfn/
